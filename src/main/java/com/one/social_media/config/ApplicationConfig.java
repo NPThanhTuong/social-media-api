46e4d86823a2a479846b7bb20d23d698c60e46ba
TranHo-003 <hnbt03@gmail.com>
2024-10-12 21:08:05 +0700

    update

--- src/main/java/com/one/social_media/config/ApplicationConfig.java
+++ src/main/java/com/one/social_media/config/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class ApplicationConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
